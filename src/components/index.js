d6e67a242c2094a5f8a55bad3a641fa555e1285c
alan-wu <alan.wu@auckland.ac.nz>
2020-04-17 10:36:51 +1200

    Change the artefact to only include the map app area.

--- src/components/index.js
+++ src/components/index.js
@@ -1,11 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from "vue";
-import MapIntegratedVuer from "./MapIntegratedVuer.vue";
+import MapContent from './MapContent.vue';
 
 
 const Components = {
-    MapIntegratedVuer
+    MapContent
   };
   
   Object.keys(Components).forEach(name => {
