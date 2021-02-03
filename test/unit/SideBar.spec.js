804c6fa0c2d91af19ffa5e6a4181565fc075c92a
alan-wu <alan.wu@auckland.ac.nz>
2021-02-04 11:52:57 +1300

    Add new unit tests.

--- test/unit/SideBar.spec.js
+++ test/unit/SideBar.spec.js
@@ -1,6 +1,7 @@
 import { shallowMount } from '@vue/test-utils'
 import SideBar from '../../src/components/SideBar';
 import SearchFilters from '../../src/components/SearchFilters';
+import DatasetCard from '../../src/components/DatasetCard';
 
 const div = document.createElement('div');
 document.body.appendChild(div);
@@ -11,5 +12,8 @@ const wrapper = shallowMount(SideBar, {
 describe('SideBar', () => {
   it('SearchFilters', () => {
     expect(wrapper.findComponent(SearchFilters).exists()).to.be.true;
+  }),
+  it('DatasetCard', () => {
+    expect(wrapper.findComponent(DatasetCard).exists()).to.be.false;
   })
 })
