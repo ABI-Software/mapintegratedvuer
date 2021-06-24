8b5800093298bdfde9c31d75862c317910c83aec
alan-wu <alan.wu@auckland.ac.nz>
2021-06-24 16:13:09 +1200

    Fix a bug with an endpoints.

--- src/components/ContentVuer.vue
+++ src/components/ContentVuer.vue
@@ -135,8 +135,11 @@ export default {
   },
   created: function() {
     this.flatmapAPI = undefined;
+    this.apiLocation = undefined;
     if (store.state.settings.flatmapAPI)
       this.flatmapAPI = store.state.settings.flatmapAPI;
+    if (store.state.settings.api)
+      this.apiLocation = store.state.settings.api;
   },
   mounted: function() {
     if (this.entry.type === 'Scaffold') {
