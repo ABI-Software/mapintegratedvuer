70f5d5721b3221374711c9aeed48c505936d7461
Alan Garny <agarny@hellix.com>
2021-08-11 11:26:47 +1200

    Some minor cleaning up.

--- src/components/ContentVuer.vue
+++ src/components/ContentVuer.vue
@@ -18,7 +18,8 @@
         :render="entry.mode !== 'minimised'" :displayMinimap=false :displayMarkers=false />
       <PlotVuer v-else-if="entry.type === 'Plot'" :url="entry.resource"
         :plotType="entry.plotType" :helpMode="helpMode" style="overflow: hidden"></PlotVuer>
-      <SimulationVuer v-else-if="entry.type === 'Simulation'" :apiLocation="apiLocation" :resource="entry.resource" :dataset="entry.dataset"></SimulationVuer>
+      <SimulationVuer v-else-if="entry.type === 'Simulation'"
+        :apiLocation="apiLocation" :resource="entry.resource" :dataset="entry.dataset" />
       <IframeVuer v-else-if="entry.type === 'Iframe'" :url="entry.resource" />
     </div>
   </div>
