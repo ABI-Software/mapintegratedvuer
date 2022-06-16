f74f51d3a6d78cf33b3ff8bc54be5ce22ffaaffd
alan-wu <alan.wu@auckland.ac.nz>
2022-06-16 13:56:24 +1200

    Complete e2e testing,

--- cypress/integration/map-app/MapContent.spec.js
+++ cypress/integration/map-app/MapContent.spec.js
@@ -17,17 +17,9 @@ describe('MapContent', () => {
     //Intercept any initial response with preloaded fixture
     cy.get('@stub').then((stub) => {
 
-      //cy.intercept('/sparc-api/get-facets/species', {statusCode: 200, body: stub.speciesResponse});
-
-      //cy.intercept('/sparc-api/get-facets/gender', {statusCode: 200, body: stub.genderResponse});
-
-      //cy.intercept( '/sparc-api/get-facets/organ', {statusCode: 200, body: stub.organResponse});
-
-      //cy.intercept('/sparc-api/filter-search/*', {statusCode: 200, body: stub.noResponse});
-
       cy.intercept('/sparc-api/dataset_info/using_multiple_dois/*', {statusCode: 200, body: stub.noResponse});
 
-      cy.intercept('/sparc-api/dataset_info/using_multiple_dois/?dois=*', {statusCode: 200, body: stub.resultResponse}).as("mouseScaffold");
+      cy.intercept('/sparc-api/dataset_info/using_multiple_dois/?dois=*', {statusCode: 200, body: stub.resultResponse}).as("mouseDataset");
     
       cy.intercept('/sparc-api/get-organ-curies?', {statusCode: 200, body: stub.curiesResponse}).as("curieResponse");
     })
@@ -65,43 +57,108 @@ describe('MapContent', () => {
 
     cy.get('.header').should('be.visible');
 
+    cy.get('.singlepanel-1 > .toolbar-flex-container > .title').should('exist');
+
     //Only three visible button on the toolbar at the start
     cy.get('.icon-group .map-icon:visible ').should('have.length', 3);
 
     //Sidebar should not be visbile
     cy.get('.el-drawer.rtl.my-drawer').should('not.be.visible');
 
-    //cy.get('.multi-container > .el-loading-parent--relative > .el-loading-mask', {timeout: 30000}).should('have.length', 0);
-
-    //Check for open button for sidebar
-
-
+    //Wait for curie response
     cy.wait('@curieResponse', {timeout: 20000});
 
-    cy.get('.content-container > .el-button').should('be.visible').click({force: true});
+    //Search for non existance feature, expect not-found text
+    cy.get('.search-box > .el-input__inner').should('exist').type("NON_EXISTANCE");
+    cy.get('.toolbar-flex-container > .map-icon > use').should('exist').click();
+    cy.get('.not-found-text').should('exist');
 
-    cy.get('.open-tab > .el-icon-arrow-left').should('exist').click();
+    //Search for Vague nerve, expect not-found text to be gone
+    cy.get('.search-box > .el-input__inner').should('exist').type("'Vagus Nerve");
+    cy.get('.toolbar-flex-container > .map-icon > use').should('exist').click();
+    cy.get('.not-found-text').should('not.exist');
 
-    cy.get('.search-input > .el-input__inner').should('exist').type('76 generic');
+    //Intercept the request and stub it with preloaded fixture
+    cy.get('@metadata').then((metadata) => {
+      cy.intercept('/WholeBody/31-May-2021/ratBody/ratBody_syncmap_metadata.json',
+        {statusCode: 200, body: metadata});
+    })
+    
+    //Intercept the request and stub it with preloaded fixture
+    cy.get('@primitive').then((primitive) => {
+      cy.intercept('/WholeBody/31-May-2021/ratBody/cube_2.json',
+        {statusCode: 200, body: primitive}).as("scaffoldResponse");
+    })
+    
+    //Click on the Open 3D Scaffold button
+    cy.get('.content-container > .el-button').should('be.visible').click();
 
-    cy.get('.header > .el-button').should('exist').click();
-
+    //Check for two content containers
     cy.get('.content-container').should('have.length', 2);
 
-    cy.wait('@mouseScaffold', {timeout: 20000});
+    //Wait for the mouse dataset request
+    cy.wait('@mouseDataset', {timeout: 20000});
 
-    cy.get('.box-card .container button').should('have.length', 3);
+    //Open the sidebar
+    cy.get('.open-tab').should('exist').click();
 
-    cy.get('.box-card .container button').contains('Scaffolds (3)').click();
+    //Type in 76 generic
+    cy.get('.search-input > .el-input__inner').should('exist').type('76 generic');
 
+    //Search
+    cy.get('.header > .el-button').should('exist').click();
+
+    //Check number of dataset card, it should be 1
+    cy.get('.dataset-card').should('have.length', 1);
+
+    //Check how many tags in the dataset
+    cy.get('.box-card .container button').should('have.length', 5);
+
+    //Intercept the request and stub it with preloaded fixture
+    cy.get('@metadata').then((metadata) => {
+      cy.intercept('/sparc-api/s3-resource/999/1/files/derivative/sub-54-8/scaffold/54-8_metadata.json',
+        {statusCode: 200, body: metadata});
+    })
+    
+    //Intercept the request and stub it with preloaded fixture
+    cy.get('@primitive').then((primitive) => {
+      cy.intercept('/sparc-api/s3-resource/999/1/files/derivative/sub-54-8/scaffold/cube_2.json',
+        {statusCode: 200, body: primitive}).as("scaffoldResponse");
+    })
+
+    //Check for scaffolds and open it, should have three items in select now
+    cy.get('.box-card .container button').contains('Scaffolds (2)').click();
     cy.get('.gallery-strip').contains('54-8_metadata.json').should("exist");
-
     cy.get('.box-card :nth-child(1) > .details .el-button').click();
-
     cy.get('.singlepanel-1.contentvuer').should('have.length', 1);
-
-    cy.get('.singlepanel-1 .el-input__inner').should('exist').click();
-
+    cy.get('.singlepanel-1 > .toolbar-flex-container > .el-select > .el-input > .el-input__inner').should('exist').click();
     cy.get('.singlepanel-1 .el-scrollbar__view >').should('have.length', 3);
+
+    //Check for segmentations and open it, should have four items in select now
+    cy.get('.box-card .container button').contains('Segmentations (1)').click();
+    cy.get('.gallery-strip').contains('M54-8_03_20_20_Final.xml').should("exist");
+    cy.get('.box-card .container button').contains('Plots (1)').click();
+    cy.get('.gallery-strip').contains('RAGP_4subs_negdct.csv').should("exist");
+    cy.get('.box-card :nth-child(1) > .details .el-button').click();
+    cy.get('.singlepanel-1 > .toolbar-flex-container > .el-select > .el-input > .el-input__inner').should('exist').click();
+    cy.get('.singlepanel-1 .el-scrollbar__view >').should('have.length', 4);
+
+    //Check for simulations and open it, should have five items in select now
+    cy.get('.box-card .container button').contains('Simulations (1)').click();
+    cy.get('.box-card :nth-child(1) > .details .el-button').click();
+    cy.get('.singlepanel-1 > .toolbar-flex-container > .el-select > .el-input > .el-input__inner').should('exist').click();
+    cy.get('.singlepanel-1 .el-scrollbar__view >').should('have.length', 5);
+
+    //Close the sidebar
+    cy.get('.close-tab').should('exist').click();
+    cy.get('.sidebar-container').should('not.be.visible');
+
+    //Change from single panel to four panels and check for it
+    cy.get('.icon-group > :nth-child(2)').should('exist').click();
+    cy.get('.icon-group.el-row .el-popover:visible').should('exist');
+    cy.get('.icon-group.el-row .el-popover:visible .el-row').should('have.length', 5);
+    cy.get('.icon-group.el-row .el-popover:visible .el-row').contains('Four panes').should('exist').click();
+    cy.get('.content-container:visible').should('have.length', 4);
+
   })
 })
