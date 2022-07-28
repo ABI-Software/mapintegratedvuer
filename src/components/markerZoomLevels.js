b43d53cceb5445f0cc3079531cef10eeb98c35bb
Jesse Khorasanee <jessekhorasanee@gmail.com>
2022-07-28 21:38:48 +1200

    Add a few missing markers

--- src/components/markerZoomLevels.js
+++ src/components/markerZoomLevels.js
@@ -5,216 +5,252 @@ let mesoscopic = 5
 // Set what level each uberon shows at
 export default [
   {
-      "id": "UBERON:0000948",
-      "name": "heart",
-      "showAtZoom": 1,
+    "id": "UBERON:0000948",
+    "name": "heart",
+    "showAtZoom": 1,
   },
   {
-      "id": "UBERON:0001759",
-      "name": "vagus nerve",
-      "showAtZoom": 1
+    "id": "UBERON:0001759",
+    "name": "vagus nerve",
+    "showAtZoom": 1
   },
   {
-      "id": "UBERON:0001155",
-      "name": "colon",
-      "showAtZoom": 1
+    "id": "UBERON:0001155",
+    "name": "colon",
+    "showAtZoom": 1
   },
   {
-      "id": "UBERON:0000945",
-      "name": "stomach",
-      "showAtZoom": 1
+    "id": "UBERON:0000945",
+    "name": "stomach",
+    "showAtZoom": 1
   },
   {
-      "id": "UBERON:0001013",
-      "name": "adipose tissue",
-      "showAtZoom": 1
+    "id": "UBERON:0001013",
+    "name": "adipose tissue",
+    "showAtZoom": 1
   },
   {
-      "id": "UBERON:0001255",
-      "name": "urinary bladder",
-      "showAtZoom": 1
+    "id": "UBERON:0001255",
+    "name": "urinary bladder",
+    "showAtZoom": 1
   },
   {
-      "id": "UBERON:0001348",
-      "name": "brown adipose tissue",
-      "showAtZoom": 6
+    "id": "UBERON:0001348",
+    "name": "brown adipose tissue",
+    "showAtZoom": 6
   },
   {
-      "id": "UBERON:0002008",
-      "name": "cardiac nerve plexus",
-      "showAtZoom": 1
+    "id": "UBERON:0002008",
+    "name": "cardiac nerve plexus",
+    "showAtZoom": 1
   },
   {
-      "id": "UBERON:0002240",
-      "name": "spinal cord",
-      "showAtZoom": 1
+    "id": "UBERON:0002240",
+    "name": "spinal cord",
+    "showAtZoom": 1
   },
   {
-      "id": "UBERON:0002005",
-      "name": "Enteric Nervous System",
-      "showAtZoom": 4
+    "id": "UBERON:0002005",
+    "name": "Enteric Nervous System",
+    "showAtZoom": 4
   },
   {
-      "id": "UBERON:0013702",
-      "name": "body proper",
-      "showAtZoom": 1
+    "id": "UBERON:0013702",
+    "name": "body proper",
+    "showAtZoom": 1
   },
   {
-      "id": "UBERON:0002298",
-      "name": "brainstem",
-      "showAtZoom": 1
+    "id": "UBERON:0002298",
+    "name": "brainstem",
+    "showAtZoom": 1
   },
   {
-      "id": "UBERON:0002439",
-      "name": "myenteric nerve plexus",
-      "showAtZoom": 6
+    "id": "UBERON:0002439",
+    "name": "myenteric nerve plexus",
+    "showAtZoom": 6
   },
   {
-      "id": "UBERON:0000010",
-      "name": "peripheral nervous system",
-      "showAtZoom": 6
+    "id": "UBERON:0000010",
+    "name": "peripheral nervous system",
+    "showAtZoom": 6
   },
   {
-      "id": "UBERON:0002048",
-      "name": "lung",
-      "showAtZoom": 1
+    "id": "UBERON:0002048",
+    "name": "lung",
+    "showAtZoom": 1
   },
   {
-      "id": "UBERON:0002410",
-      "name": "autonomic nervous system",
-      "showAtZoom": 6
+    "id": "UBERON:0002410",
+    "name": "autonomic nervous system",
+    "showAtZoom": 6
   },
   {
-      "id": "UBERON:0002441",
-      "name": "cervicothoracic ganglion",
-      "showAtZoom": 6
+    "id": "UBERON:0002441",
+    "name": "cervicothoracic ganglion",
+    "showAtZoom": 6
   },
   {
-      "id": "UBERON:0005363",
-      "name": "inferior vagus X ganglion",
-      "showAtZoom": 6
+    "id": "UBERON:0005363",
+    "name": "inferior vagus X ganglion",
+    "showAtZoom": 6
   },
   {
-      "id": "UBERON:0001264",
-      "name": "pancreas",
-      "showAtZoom": 1
+    "id": "UBERON:0001264",
+    "name": "pancreas",
+    "showAtZoom": 1
   },
   {
-      "id": "UBERON:0001884",
-      "name": "phrenic nerve",
-      "showAtZoom": 6
+    "id": "UBERON:0001884",
+    "name": "phrenic nerve",
+    "showAtZoom": 6
   },
   {
-      "id": "UBERON:0002059",
-      "name": "submandibular ganglion",
-      "showAtZoom": 7
+    "id": "UBERON:0002059",
+    "name": "submandibular ganglion",
+    "showAtZoom": 7
   },
   {
-      "id": "UBERON:0001347",
-      "name": "white adipose tissue",
-      "showAtZoom": 6
+    "id": "UBERON:0001347",
+    "name": "white adipose tissue",
+    "showAtZoom": 6
   },
   {
-      "id": "UBERON:0001556",
-      "name": "lower urinary tract",
-      "showAtZoom": 1
+    "id": "UBERON:0001556",
+    "name": "lower urinary tract",
+    "showAtZoom": 1
   },
   {
-      "id": "UBERON:0002262",
-      "name": "celiac ganglion",
-      "showAtZoom": 5
+    "id": "UBERON:0002262",
+    "name": "celiac ganglion",
+    "showAtZoom": 5
   },
   {
-      "id": "UBERON:0000955",
-      "name": "Brain",
-      "showAtZoom": 1
+    "id": "UBERON:0000955",
+    "name": "Brain",
+    "showAtZoom": 1
   },
   {
-      "id": "UBERON:0000160",
-      "name": "intestine",
-      "showAtZoom": 1
+    "id": "UBERON:0000160",
+    "name": "intestine",
+    "showAtZoom": 1
   },
   {
-      "id": "UBERON:0001322",
-      "name": "sciatic nerve",
-      "showAtZoom": 1
+    "id": "UBERON:0001322",
+    "name": "sciatic nerve",
+    "showAtZoom": 1
   },
   {
-      "id": "UBERON:0001649",
-      "name": "glossopharyngeal nerve",
-      "showAtZoom": 6
+    "id": "UBERON:0001649",
+    "name": "glossopharyngeal nerve",
+    "showAtZoom": 6
   },
   {
-      "id": "UBERON:0002063",
-      "name": "sinus venosus",
-      "showAtZoom": 6
+    "id": "UBERON:0002063",
+    "name": "sinus venosus",
+    "showAtZoom": 6
   },
   {
-      "id": "UBERON:0002107",
-      "name": "liver",
-      "showAtZoom": 1
+    "id": "UBERON:0002107",
+    "name": "liver",
+    "showAtZoom": 1
   },
   {
-      "id": "UBERON:0002113",
-      "name": "kidney",
-      "showAtZoom": 1
+    "id": "UBERON:0002113",
+    "name": "kidney",
+    "showAtZoom": 1
   },
   {
-      "id": "UBERON:0002481",
-      "name": "bone tissue",
-      "showAtZoom": 1
+    "id": "UBERON:0002481",
+    "name": "bone tissue",
+    "showAtZoom": 1
   },
   {
-      "id": "UBERON:0004907",
-      "name": "lower digestive tract",
-      "showAtZoom": 4
+    "id": "UBERON:0004907",
+    "name": "lower digestive tract",
+    "showAtZoom": 4
   },
   {
-      "id": "UBERON:0000044",
-      "name": "Dorsal root ganglion",
-      "showAtZoom": 5
+    "id": "UBERON:0000044",
+    "name": "Dorsal root ganglion",
+    "showAtZoom": 5
   },
   // New levels from Keeran
   {
-      "id": "UBERON:0001736",
-      "name": "Submandibular gland",
-      "showAtZoom": microscopic,
-      "keyword": true
+    "id": "UBERON:0001736",
+    "name": "Submandibular gland",
+    "showAtZoom": microscopic,
+    "keyword": true
   },
   {
-      "id": "UBERON:0001103",
-      "name": "Diaphragm",
-      "showAtZoom": mesoscopic,
-      "keyword": true
+    "id": "UBERON:0001103",
+    "name": "Diaphragm",
+    "showAtZoom": mesoscopic,
+    "keyword": true
   },
   {
-      "id": "UBERON:0001160",
-      "name": "Fundus of stomach",
-      "showAtZoom": mesoscopic,
-      "keyword": true
+    "id": "UBERON:0001160",
+    "name": "Fundus of stomach",
+    "showAtZoom": mesoscopic,
+    "keyword": true
   },
   {
-      "id": "UBERON:0000955",
-      "name": "Brain",
-      "showAtZoom": mesoscopic,
-      "keyword": true
+    "id": "UBERON:0000955",
+    "name": "Brain",
+    "showAtZoom": mesoscopic,
+    "keyword": true
   },
   {
-      "id": "UBERON:0003708",
-      "name": "Carotid sinus",
-      "showAtZoom": microscopic,
-      "keyword": true
+    "id": "UBERON:0003708",
+    "name": "Carotid sinus",
+    "showAtZoom": microscopic,
+    "keyword": true
   },
   {
-      "id": "UBERON:0002079",
-      "name": "Left cardiac atrium",
-      "showAtZoom": mesoscopic,
-      "keyword": true
+    "id": "UBERON:0002079",
+    "name": "Left cardiac atrium",
+    "showAtZoom": mesoscopic,
+    "keyword": true
   },
   {
-      "id": "ILX:0485722",
-      "name": "Nodose ganglion",
-      "showAtZoom": microscopic,
-      "keyword": true
+    "id": "ILX:0485722",
+    "name": "Nodose ganglion",
+    "showAtZoom": microscopic,
+    "keyword": true
+  },
+  {
+    "id": "UBERON:0002078",
+    "name": "Right cardiac atrium",
+    "showAtZoom": mesoscopic,
+    "keyword": true
+  },
+  {
+    "id": "UBERON:0002084",
+    "name": "Heart left ventricle",
+    "showAtZoom": mesoscopic,
+    "keyword": true
+  },
+  {
+    "id": "UBERON:0002080",
+    "name": "Heart right ventricle",
+    "showAtZoom": mesoscopic,
+    "keyword": true
+  },
+  {
+    "id": "UBERON:0001508",
+    "name": "Arch of Aorta",
+    "showAtZoom": mesoscopic,
+    "keyword": true
+  },
+  {
+    "id": "UBERON:0001738",
+    "name": "Thyroid cartilage",
+    "showAtZoom": mesoscopic,
+    "keyword": true
+  },
+  {
+    "id": "UBERON:0000388",
+    "name": "Epiglottis",
+    "showAtZoom": mesoscopic,
+    "keyword": true
   },
 ]
