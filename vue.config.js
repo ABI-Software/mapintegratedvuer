b3643bc1a34ff3f35b7c5e075ab54ffd6dc25434
alan-wu <alan.wu@auckland.ac.nz>
2023-06-15 12:13:31 +1200

    Fix an issue with marked not built in.

--- vue.config.js
+++ vue.config.js
@@ -34,7 +34,7 @@ module.exports = {
     if(process.env.NODE_ENV === 'production') {
       //By including element-ui and all abi projects, the problem with element-ui
       //stylesheet can be avoided.
-      config.externals =  [ nodeExternals({allowlist: [/^element-ui/, /^@abi-software/, /^physiomeportal/]}) ];
+      config.externals =  [ nodeExternals({allowlist: [/^element-ui/, /^@abi-software/, /^marked/ ]}) ];
     }
   },
   css: {
