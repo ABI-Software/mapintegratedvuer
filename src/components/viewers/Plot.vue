b28d16baf675aeffc41610764343a5d49bc03244
Alan Wu <alan.wu@auckland.ac.nz>
2024-02-08 17:25:30 +1300

    Update dependencies and version.

--- src/components/viewers/Plot.vue
+++ src/components/viewers/Plot.vue
@@ -1,25 +1,23 @@
 <template>
-  <!--
   <PlotVuer
     :data-source="entry.resource.dataSource"
     :metadata="entry.resource.metadata"
     :supplemental-data="entry.resource.supplementalData"
     style="overflow: hidden"
   />
-  -->
 </template>
 
 <script>
 /* eslint-disable no-alert, no-console */
-//import { PlotVuer } from "@abi-software/plotvuer";
-//import "@abi-software/plotvuer/dist/plotvuer.css";
+import { PlotVuer } from "@abi-software/plotvuer";
+import "@abi-software/plotvuer/dist/style.css";
 import ContentMixin from "../../mixins/ContentMixin";
 
 export default {
   name: "Plot",
   mixins: [ ContentMixin ],
   components: {
- //   PlotVuer,
+    PlotVuer,
   }
 };
 </script>
--- src/components/viewers/Simulation.vue
+++ src/components/viewers/Simulation.vue
@@ -1,23 +1,21 @@
 <template>
-  <!--
   <SimulationVuer
     :apiLocation="apiLocation"
     :id="entry.discoverId"
   />
-  -->
 </template>
 
 <script>
 /* eslint-disable no-alert, no-console */
 import ContentMixin from "../../mixins/ContentMixin";
-//import { SimulationVuer } from "@abi-software/simulationvuer";
-//import "@abi-software/simulationvuer/dist/simulationvuer.css";
+import { SimulationVuer } from "@abi-software/simulationvuer";
+import "@abi-software/simulationvuer/dist/style.css";
 
 export default {
   name: "Simulation",
   mixins: [ ContentMixin ],
   components: {
-  //  SimulationVuer,
+    SimulationVuer,
   },
 };
 </script>
