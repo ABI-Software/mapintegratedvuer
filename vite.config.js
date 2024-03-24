1bb2764d651855e5d88fa7d8a96cfd3d02dbeaec
David J. Yu <87633683+ddjnw1yu@users.noreply.github.com>
2024-03-25 09:49:38 +1300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,4 @@
 import path from "path";
-const pathSrc = path.resolve(__dirname, "./src");
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import Components from 'unplugin-vue-components/vite'
