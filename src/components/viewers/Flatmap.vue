b3179177c3896bc02ccf411d049634d31e3cbd34
David J. Yu <87633683+ddjnw1yu@users.noreply.github.com>
2024-08-08 14:26:36 +1200

    Fix a issue when enter FC map

--- src/components/viewers/Flatmap.vue
+++ src/components/viewers/Flatmap.vue
@@ -100,7 +100,7 @@ export default {
       let provClone = {id: this.entry.id, prov: this.getFlatmapImp().provenance}; //create clone of provenance and add id
       EventBus.emit("mapImpProv", provClone); // send clone to context card
       this.$emit("flatmap-provenance-ready", provClone);
-      if (this.entry.resource === "FunctionalConnectivity"){
+      if (this.entry.resource !== "FunctionalConnectivity"){
         this.flatmapReadyForMarkerUpdates(flatmap);
       }
       EventBus.emit("mapReady", flatmap);
