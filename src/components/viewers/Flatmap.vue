e5e78e6365dc0154ed214065025b5ac07e81b5e6
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2025-05-23 14:15:42 +1200

    Fix connectivity list for FC map

--- src/components/viewers/Flatmap.vue
+++ src/components/viewers/Flatmap.vue
@@ -102,9 +102,11 @@ export default {
     },
     flatmapReadyCall: function (flatmap) {
       let provClone = {id: this.entry.id, prov: this.getFlatmapImp().provenance}; //create clone of provenance and add id
+      const flatmapImp = flatmap.mapImp;
       EventBus.emit("mapImpProv", provClone); // send clone to context card
       this.$emit("flatmap-provenance-ready", provClone);
       this.flatmapReadyForMarkerUpdates(flatmap);
+      this.loadConnectivityKnowledge(flatmapImp);
       EventBus.emit("mapLoaded", flatmap);
     },
     onPathwaySelectionChanged: function (data) {
