f48ec9a2bf7b6f416d97707ca0aabea959c0fdcc
alan-wu <alan.wu@auckland.ac.nz>
2025-06-16 12:26:13 +1200

    Temporary workaround an issue with eventbus on viewer.

--- src/stores/splitFlow.js
+++ src/stores/splitFlow.js
@@ -370,7 +370,7 @@ export const useSplitFlowStore = defineStore('splitFlow', {
             availableId == 0; i++) {
             //Find the first entry not currently in use
             if ((payload.entries[i].id !== payload.id) &&
-              findKeyWithId(payload.entries[i].id) === undefined) {
+              findKeyWithId(this.customLayout, payload.entries[i].id) === undefined) {
               availableId = payload.entries[i].id;
             }
           }
@@ -402,7 +402,7 @@ export const useSplitFlowStore = defineStore('splitFlow', {
           }
           const customLayout = newLayoutWithOrigInfo(
             this.customLayout, this.activeView);
-          const key = findKeyWithId(customLayout, payload.id);
+          const key = findKeyWithId(this.customLayout, payload.id);
           // The following move the entry id to the appropriate slot
           // and remove the target id
           switch (key) {
@@ -556,7 +556,6 @@ export const useSplitFlowStore = defineStore('splitFlow', {
             this.customLayout[key] = value;
           }
         }
-        this.updateSplitPanels();
       }
     },
     updateSplitPanels() {
