a0615d1676a9d00b65846c6bd52cbc441945d364
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2025-08-12 11:39:23 +1200

    Update getFlatmapFilters

--- src/mixins/ContentMixin.js
+++ src/mixins/ContentMixin.js
@@ -11,13 +11,11 @@ import { useConnectivitiesStore } from '../stores/connectivities';
 import Tagging from '../services/tagging.js';
 
 import {
-  extractOriginItems,
-  extractDestinationItems,
-  extractViaItems,
+  getFlatmapFilterOptions,
 } from '@abi-software/map-utilities'
 import { FlatmapQueries } from "@abi-software/flatmapvuer/src/services/flatmapQueries.js";
 import { getKnowledgeSource, loadAndStoreKnowledge } from "@abi-software/flatmapvuer/src/services/flatmapKnowledge.js";
-import { getTermNerveMaps, getFilterOptions } from "@abi-software/scaffoldvuer/src/scripts/MappedNerves.js";
+import { getTermNerveMaps, getFilterOptions as getScaffoldFilterOptions } from "@abi-software/scaffoldvuer/src/scripts/MappedNerves.js";
 
 function capitalise(text) {
   return text[0].toUpperCase() + text.substring(1)
@@ -571,154 +569,6 @@ export default {
       this.flatmapService = await this.mockUpFlatmapService();
       this.loadConnectivityExplorerConfig(this.flatmapService);
     },
-    // TODO: to update getFilterOptions from flatmapvuer
-    getFlatmapFilterOptions: async function (mapImp, providedKnowledge) {
-      let filterOptions = [];
-      const connectionFilters = [];
-      if (mapImp) {
-        // get flatmap filters
-        if (mapImp && typeof mapImp.featureFilterRanges === 'function') {
-          const filterRanges = mapImp.featureFilterRanges();
-          for (const [key, value] of Object.entries(filterRanges)) {
-            let main = {
-              key: `flatmap.connectivity.${key}`,
-              label: "",
-              children: []
-            }
-            let children = []
-            if (key === "kind") {
-              main.label = "Pathways"
-              for (const facet of value) {
-                const pathway = this.pathways.find(path => path.type === facet)
-                if (pathway) {
-                  children.push({
-                    key: `${main.key}.${facet}`,
-                    label: pathway.label,
-                    colour: pathway.colour,
-                    colourStyle: 'line',
-                    dashed: pathway.dashed,
-                  })
-                }
-              }
-            } else if (key === "taxons") {
-              main.label = "Studied in"
-              const entityLabels = await findTaxonomyLabels(mapImp, mapImp.taxonIdentifiers)
-              if (entityLabels.length) {
-                for (const facet of value) {
-                  const taxon = entityLabels.find(p => p.taxon === facet)
-                  if (taxon) {
-                    children.push({
-                      key: `${main.key}.${facet}`,
-                      // space added at the end of label to make sure the display name will not be updated
-                      // prevent sidebar searchfilter convertReadableLabel
-                      label: `${taxon.label} `
-                    })
-                  }
-                }
-              }
-            } else if (key === "alert") {
-              main.label = "Alert"
-              for (const facet of ["with", "without"]) {
-                children.push({
-                  key: `${main.key}.${facet}`,
-                  label: `${facet} alerts`
-                })
-              }
-            }
-            main.children = children.sort((a, b) => a.label.localeCompare(b.label));
-            if (main.label && main.children.length) {
-              filterOptions.push(main)
-            }
-          }
-        }
-
-        const baseFlatmapKnowledge = providedKnowledge || this.getFlatmapKnowledge();
-        const mapKnowledge = mapImp.pathways.paths;
-        const flatmapKnowledge = baseFlatmapKnowledge.reduce((arr, knowledge) => {
-          const id = knowledge.id;
-          if (id) {
-            const mapKnowledgeObj = mapKnowledge[id];
-            if (mapKnowledgeObj && mapKnowledgeObj.connectivity && mapKnowledgeObj['node-phenotypes']) {
-              const mapConnectivity = mapKnowledgeObj.connectivity;
-              const mapNodePhenotypes = mapKnowledgeObj['node-phenotypes'];
-              // take only map connectivity
-              knowledge.connectivity = [...mapConnectivity];
-              for (let key in knowledge['node-phenotypes']) {
-                if (mapNodePhenotypes[key]) {
-                  // take only map node-phenotypes
-                  knowledge['node-phenotypes'][key] = [...mapNodePhenotypes[key]];
-                }
-              }
-              // to avoid mutation
-              arr.push(JSON.parse(JSON.stringify(knowledge)));
-            }
-          }
-          return arr;
-        }, []);
-        const knowledgeSource = mapImp.knowledgeSource;
-        const originItems = await extractOriginItems(this.flatmapAPI, knowledgeSource, flatmapKnowledge);
-        const viaItems = await extractViaItems(this.flatmapAPI, knowledgeSource, flatmapKnowledge);
-        const destinationItems = await extractDestinationItems(this.flatmapAPI, knowledgeSource, flatmapKnowledge);
-
-        const transformItem = (facet, item) => {
-          const key = JSON.stringify(item.key);
-          return {
-            key: `flatmap.connectivity.source.${facet}.${key}`,
-            label: item.label || key
-          };
-        }
-
-        for (const facet of ["origin", "via", "destination", "all"]) {
-          let childrenList = []
-          if (facet === 'origin') {
-            childrenList = originItems.map((item) => transformItem(facet, item));
-          } else if (facet === 'via') {
-            childrenList = viaItems.map((item) => transformItem(facet, item));
-          } else if (facet === 'destination') {
-            childrenList = destinationItems.map((item) => transformItem(facet, item));
-          } else {
-            // All is the combination of origin, via, destination
-            const allList = [
-              ...originItems.map((item) => transformItem(facet, item)),
-              ...viaItems.map((item) => transformItem(facet, item)),
-              ...destinationItems.map((item) => transformItem(facet, item))
-            ];
-            // Generate unique list since the same feature can be in origin, via, and destination
-            const seenKeys = new Set();
-            childrenList = allList.filter(item => {
-              if (seenKeys.has(item.key)) return false;
-              seenKeys.add(item.key);
-              return true;
-            });
-          }
-
-          // Those without label but key should be below
-          childrenList = childrenList.sort((a, b) => {
-            const isAlpha = (str) => /^[a-zA-Z]/.test(str);
-            const aAlpha = isAlpha(a.label);
-            const bAlpha = isAlpha(b.label);
-
-            if (aAlpha && !bAlpha) return -1;
-            if (!aAlpha && bAlpha) return 1;
-
-            return a.label.localeCompare(b.label);
-          });
-
-          if (childrenList.length) {
-            connectionFilters.push({
-              key: `flatmap.connectivity.source.${facet}`,
-              label: facet,
-              children: childrenList,
-            })
-          }
-        }
-
-        if (connectionFilters.length) {
-          filterOptions.push(...connectionFilters)
-        }
-      }
-      return filterOptions;
-    },
     mockUpFlatmapService: async function() {
       const flatmapResponse = await fetch(this.flatmapAPI);
       const flatmapJson = await flatmapResponse.json();
@@ -769,8 +619,10 @@ export default {
         },
       };
 
-      const flatmapFilterOptions = await this.getFlatmapFilterOptions(mapImp, this.connectivityKnowledge[flatmapUuid]);
-      const scaffoldFilterOptions = getFilterOptions();
+      const providedKnowledge = this.connectivityKnowledge[flatmapUuid];
+      const providedPathways = undefined;
+      const flatmapFilterOptions = await getFlatmapFilterOptions(this.flatmapAPI, mapImp, providedKnowledge, providedPathways);
+      const scaffoldFilterOptions = getScaffoldFilterOptions();
       const combinedFilterOptions = () => [...scaffoldFilterOptions, ...flatmapFilterOptions];
 
       return {
@@ -800,7 +652,7 @@ export default {
           .filter(item => item.id in pathways);
       }
       if (!this.connectivityFilterOptions[uuid]) {
-        this.connectivityFilterOptions[uuid] = await flatmap.getFilterOptions(this.connectivityKnowledge[uuid]);
+        this.connectivityFilterOptions[uuid] = await flatmap.getFilterOptions(flatmapImp , this.connectivityKnowledge[uuid]);
       }
       if (flatmap.mockup) {
         const nerveMaps = flatmap.getTermNerveMaps() || {};
