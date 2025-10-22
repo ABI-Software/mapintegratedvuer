a370bc8ce68898fb0b30f589cbccc4085a28d17f
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2025-10-23 11:49:38 +1300

    Move cypress commands into commands file

--- cypress/component/NeuronConnection.cy.js
+++ cypress/component/NeuronConnection.cy.js
@@ -14,46 +14,6 @@ describe('MapContent', () => {
     cy.fixture('stubResponse.json').as('stub');
   })
 
-  Cypress.on('uncaught:exception', (err) => {
-    // returning false here prevents Cypress from
-    // failing the test
-    if (err.message.includes("this.$refs.sideBar.closeConnectivity is not a function"))
-      return false
-    if (err.message.includes("Cannot read properties of undefined (reading 'left')"))
-      return false
-    if (err.message.includes("Failed to fetch"))
-      return false
-    if (err.message.includes('Source "mapbox-gl-draw-cold" already exists.'))
-      return false
-    if (err.message.includes('Source "markers" already exists.'))
-      return false
-    if (err.message.includes("Cannot read properties of undefined (reading 'onResize')"))
-      return false
-    if (err.message.includes("knowledge/query/")) {
-      return false
-    }
-    if (err.message.includes("Cannot read properties of null (reading 'id')")) {
-      return false
-    }
-    if (err.message.includes("$el.css is not a function")) {
-      return false
-    }
-    return true
-  })
-
-  Cypress.Commands.add('checkNeuronConnectionMode', (mode, searchTerm) => {
-    cy.get('.viewing-mode-selector .el-dropdown').as('viewingModes').trigger('mouseenter'); // open
-    cy.get('@viewingModes').contains(mode).click();
-    cy.get('.search-box.el-autocomplete > .el-input > .el-input__wrapper > .el-input__inner').should('exist').type(searchTerm);
-    cy.get('.search-container > .map-icon > use').should('exist').click();
-    cy.wait(2000);
-    const tagTerm = `${mode[0]}:${searchTerm}`
-    cy.get('.sidebar-container .filters').should('exist').contains(tagTerm);
-    cy.get('.connectivity-card-container > .connectivity-card').should('have.length.greaterThan', 0);
-    cy.get('.sidebar-container .el-card:visible .header .is-link > span').contains('Reset').click({ multiple: true })
-    cy.get('.search-box.el-autocomplete > .el-input > .el-input__wrapper > .el-input__inner').should('exist').clear();
-  })
-
   it('Neuron connection mode testing', () => {
 
     //Intercept any initial response with preloaded fixture
