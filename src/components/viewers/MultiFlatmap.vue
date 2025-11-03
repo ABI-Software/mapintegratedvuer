0faf635226fd738016985a44f3184cf4008e4099
alan-wu <alan.wu@auckland.ac.nz>
2025-11-03 15:45:48 +1300

    Provide a workaround with bug found on source changed.

--- src/components/viewers/MultiFlatmap.vue
+++ src/components/viewers/MultiFlatmap.vue
@@ -359,10 +359,14 @@ export default {
         }
       }
     },
-    changeConnectivitySource: function (payload) {
+    changeConnectivitySource: function (payload, ongoingSource) {
       if (this?.alive && this.flatmapReady) {
         const flatmap = this.$refs.multiflatmap.getCurrentFlatmap();
-        flatmap.changeConnectivitySource(payload);
+        const flatmapUUID = flatmap.mapImp.mapMetadata.uuid;
+        if (!ongoingSource.includes(flatmapUUID)) {
+          ongoingSource.push(flatmapUUID);
+          flatmap.changeConnectivitySource(payload);
+        }
       }
     },
     updateViewerSettings: function () {
