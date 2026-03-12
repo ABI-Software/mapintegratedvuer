547179bdf61bee051ed3070e8977671e34bc075f
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2026-03-12 19:15:05 +1300

    Add a sample connectivity graph

--- src/components/viewers/ConnectivityGraph.vue
+++ src/components/viewers/ConnectivityGraph.vue
@@ -1,20 +1,70 @@
 <template>
-  <h3>Connectivity Graph</h3>
-  <!-- <connectivity-graph /> -->
+  <div class="viewer-container">
+    <MapUtilitiesConnectivityGraph
+      :entry="graphEntry"
+      :knowledgeData="sampleKnowledge"
+      :connectivityError="{}"
+      @tap-node="onTapNode"
+    />
+  </div>
 </template>
 
 <script>
 /* eslint-disable no-alert, no-console */
 import {
-  ConnectivityGraph,
+  ConnectivityGraph as MapUtilitiesConnectivityGraph,
 } from '@abi-software/map-utilities';
 import ContentMixin from "../../mixins/ContentMixin";
+
+const SAMPLE_KNOWLEDGE = {
+  dendrites: [
+    ['UBERON:0002107', ['ILX:sample-origin']],
+  ],
+  somas: [
+    ['UBERON:0000955', ['ILX:sample-component']],
+  ],
+  axons: [
+    ['UBERON:0004535', ['ILX:sample-destination']],
+  ],
+  connectivity: [
+    [
+      ['UBERON:0002107', ['ILX:sample-origin']],
+      ['UBERON:0000955', ['ILX:sample-component']],
+    ],
+    [
+      ['UBERON:0000955', ['ILX:sample-component']],
+      ['UBERON:0004535', ['ILX:sample-destination']],
+    ],
+  ],
+};
 
 export default {
   name: "ConnectivityGraph",
   mixins: [ ContentMixin ],
   components: {
-    ConnectivityGraph,
-  }
+    MapUtilitiesConnectivityGraph,
+  },
+  computed: {
+    graphEntry: function () {
+      return this.entry?.resource || 'sample:connectivity-graph';
+    },
+    sampleKnowledge: function () {
+      return SAMPLE_KNOWLEDGE;
+    },
+  },
+  methods: {
+    onTapNode: function () {
+      return;
+    },
+  },
 };
 </script>
+
+<style scoped lang="scss">
+.viewer-container {
+  width: 100%;
+  height: 100%;
+  padding: 1rem;
+  box-sizing: border-box;
+}
+</style>
