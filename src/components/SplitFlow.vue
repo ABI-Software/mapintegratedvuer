753fd7e993a8e340b112f695c1c94c1d3c295c6a
akhuoa <aung.kyaw.hein@auckland.ac.nz>
2026-07-08 15:36:51 +1200

    Remove CORS proxy

--- src/components/SplitFlow.vue
+++ src/components/SplitFlow.vue
@@ -1092,7 +1092,6 @@ export default {
         PENNSIEVE_API_LOCATION: this.settingsStore.pennsieveApi,
         ROOT_URL: this.settingsStore.rootUrl,
         FLATMAPAPI_LOCATION: this.settingsStore.flatmapAPI,
-        CORS_PROXY_API: import.meta.env.VITE_CORS_PROXY_API,
       };
     },
     entries: function() {
